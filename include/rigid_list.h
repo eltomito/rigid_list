39af920081218828e2dfb3766c240de4a4e07f9e
Tomas Partl <tomaspartl@centrum.cz>
2015-12-21 17:41:22 +0100

    Documentation updates.

--- include/rigid_list.h
+++ include/rigid_list.h
@@ -8,15 +8,17 @@
 #define abs( n ) ((n >= 0) ? n : n*(-1))
 #endif //abs
 
-/** A version of std::list that only allocates memory when you tell it to.
+/**
+ * \mainpage
+ * A version of std::list that only allocates memory when you tell it to.
  *
- * This is the same as std::list except that it only uses preallocated memory and fail to grow when it runs out of it.
- * Use reserve( n ) to preallocate memory for n list items.
- * Then you can push_back() up to n items and they will be stored in the list
+ * This is the same as std::list except that it only uses preallocated memory and fails to grow when it runs out of it.
+ * Use reserve( n ) to preallocate memory for n list elements.
+ * Then you can push_back() up to n elements and they will be stored in the list
  * without the list allocating any new memory.
- * If you push_back() more items beyond n+1, they will not be added and push_back() will return false.
+ * If you push_back() more elements beyond n+1, they will not be added and push_back() will return false.
  *
- * You can increase the capacity of the list by calling reserve( m ) where m > n and continue push_back()-ing more items into it.
+ * You can increase the capacity of the list by calling reserve( m ) where m > n and continue push_back()-ing more elements into it.
  *
  * BEWARE!
  * Not all methods of std::list and their various versions are implemented.
@@ -36,15 +38,15 @@ template <class T> class rigid_list : public std::list<T>
 private:
 	typedef	std::list<T> free_listT;
 public:
-	/** Constructor - n is the number of items to preallocate.
+	/** Constructor - n is the number of elements to preallocate.
 	*/
 	rigid_list( size_t n = 0 ) : std::list<T>() { reserve( n ); };
 
-	/** Preallocate memory for n list items.
-	 * @param n - The number of items memory should be allocated for.
+	/** Preallocate memory for n list elements.
+	 * @param n - The number of elements memory should be allocated for.
 	 *
 	 * If n is smaller than the current size of the list,
-    * some items will be dropped from the end of the list.
+    * elements at the end that don't fit in the new capacity will be dropped.
 	 */
 	void reserve( size_t n )
 	{
@@ -68,8 +70,8 @@ public:
 		}
 	};
 
-	/** Add an item to the end of the list.
- 	 * @returns true if there was enough space to add the item, false otherwise.
+	/** Add an element to the end of the list.
+ 	 * @returns true if there was enough space to add the element, false otherwise.
 	 */
 	bool push_back( const T &item )
 	{
@@ -81,8 +83,8 @@ public:
 		return false;
 	};
 
-	/** Add an item to the beginning of the list.
- 	 * @returns true if there was enough space to add the item, false otherwise.
+	/** Add an element to the beginning of the list.
+ 	 * @returns true if there was enough space to add the element, false otherwise.
 	 */
 	bool push_front( const T &item )
 	{
@@ -106,7 +108,7 @@ public:
 		return pos;
 	};
 
-	/** Insert a number of copies of an item at a given position.
+	/** Insert a number of copies of an element at a given position.
 	* @returns true if there was enough free space to do it, false otherwise.
  	*/
 	bool insert( typename rigid_list<T>::iterator pos, size_t count, const T& item )
@@ -126,9 +128,9 @@ public:
 	* When splicing elements from the same list, it works exactly the same as in a std::list.
 	* If the source list is different, then the operation is only carried out
 	* if capacities of both lists can be preserved, i.e., if the destination list
-	* doesn't have enough free space to exchange for the new item, the operation fails.
+	* doesn't have enough free space to exchange for the new element, the operation fails.
 	*
-	* @returns true if splicing from the same list or if there was enough free elements to exchange for.
+	* @returns true if splicing from the same list or if there was enough free elements to exchange for the new ones.
 	*          False if splicing from another list and not enough free space was available.
    */
 	bool splice( typename rigid_list<T>::iterator pos, rigid_list<T> &other, typename rigid_list<T>::iterator it )
@@ -161,7 +163,7 @@ public:
 		return true;
 	};
 
-	/** Deletes the last item from the list.
+	/** Deletes the last element from the list.
 	*/
 	void pop_back()
 	{
@@ -171,7 +173,7 @@ public:
 		free_list.splice( free_list.end(), (*this), it );
 	};
 
-	/** Deletes the first item from the list.
+	/** Deletes the first element from the list.
 	*/
 	void pop_front()
 	{
@@ -179,33 +181,33 @@ public:
 		free_list.splice( free_list.end(), (*this), this->begin() );
 	};
 
-	/** Erase one item from the list.
+	/** Erase one element from the list.
 	* The capacity of the list is unaffected.
 	*/
 	void erase( typename rigid_list<T>::iterator it ) {
 		free_list.splice( free_list.end(), *this, it );
 	};
 
-	/** Erase several items from the list.
+	/** Erase several elements from the list.
 	* The capacity of the list is unaffected.
 	*/
 	void erase( typename rigid_list<T>::iterator its, typename rigid_list<T>::iterator ite ) {
 		free_list.splice( free_list.end(), *this, its, ite );
 	};
 
-	/** Erase all items from the list.
+	/** Erase all elements from the list.
 	* The capacity of the list is unaffected.
 	*/
 	void clear() {
 		this->erase( this->begin(), this->end() );
 	};
 
-	/** Report the maximum number of items this list can currently hold.
+	/** Report the maximum number of elements this list can currently hold.
 	* The capacity can be changed by calling reserve().
 	*/
 	size_t capacity() { return this->size() + free_list.size(); };
 
-	/** Report the number of items that can still be added to this list before its capacity is filled up.
+	/** Report the number of elements that can still be added to this list before its capacity is filled up.
 	*/
 	size_t free_size() { return free_list.size(); };
 
